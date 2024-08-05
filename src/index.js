bb0703c1d61af8c6865abf45f36b7544ab556c5b
Jenisha Khulal <jenishakhulal@gmail.com>
2024-08-05 15:26:00 +0545

    error format and asynchandler added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000;
 
 dotenv.config({
     path:".env"
 })
 
 connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening at ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection failed:", error);
+  });
